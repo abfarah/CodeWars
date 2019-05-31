802d7f835874ba33edb3996dedd36f9c64ba79ab
Abdullahi Farah <Abdullahi.farah96@gmail.com>
2019-05-31 15:29:25 -0500

    Solved solution and created unit test

---
+++ java/8kyu/Multiply/MultiplyTest.java
@@ -0,0 +1,26 @@
+/*
+
+By Abdullahi Farah
+
+Unit Tests for codewars vowel problem
+
+Dependencies: 
+		junit-4.10.jar
+		hamcrest-core-2.1-rc4.jar
+
+
+To Test: javac -cp .:junit-4.10.jar MultiplyTest.java
+To Rum: java -cp .:junit-4.10.jar:hamcrest-core-2.1-rc4.jar org.junit.runner.JUnitCore MultiplyTest.java
+*/
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MultiplyTest {
+    
+    @Test
+    public void testCase1() {
+      assertEquals("Nope!", 25, Multiply.multiply(5,5));
+    }
+    
+}
